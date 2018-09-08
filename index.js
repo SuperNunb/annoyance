bf4532d9e55c7fc4737456d24d8c39f41f812bce
SuperNunb <33640719+SuperNunb@users.noreply.github.com>
2018-09-08 08:05:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,9 +29,8 @@ bot.on("message", message => {
         bot.user.setStatus('dnd');
     } else if (annoySwitch == true && message.author.id != "467677680251305984" && message.attachments.size <= 0) {
         message.channel.send(message.content);
-    } else if (message.content.startsWith("annoy") && /*message.content.includes("@")*/message.mentions.array[0] != null) {
+    } else if (message.content.startsWith("annoy") && /*message.content.includes("@")*/message.mentions.array()[0] != null) {
          if (message.member.hasPermission("MENTION_EVERYONE")) {
-             //console.log(message.mentions.roles.array()[0]);
              if (message.mentions.roles.array()[0] != null) {
                  message.channel.send(`You can only annoy users, ${message.author}.`);
                  return;
